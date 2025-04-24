9e88196df9431dc67ce38ee1aac955967e6c8c1c
kavin766 <codehub676@gmail.com>
2025-04-24 16:00:15 +0530

    unit-1

---
+++ unit-1/dd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, re, i;
+    /*for (i=0;i<=2;i++)
+    {
+    printf("Enter your number :");
+    scanf("%d", &num1);
+    }
+    scanf("%d", &num2);*/
+    printf("Enter your number1 :");
+    scanf("%d", &num1);
+    printf("Enter your number2 :");
+    scanf("%d", &num2);
+    re = num1 + num2;
+    
+    printf("%d\n", re);
+
+
+}
